0f4f24cbda4f66067889c074fdd266af06ecd43b
lups-717 <lcastilho717@gmail.com>
2023-10-17 09:55:13 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+    {
+      "id": "1",
+      "photo":"GOW",
+      "title":"God Of War Ragnarock é um sucesso",
+      "description":"God of War: Ragnarok, um dos principais exclusivos do PlayStation 4 (PS4) e PlayStation 5 (PS5) de 2022, bateu recorde de vendas em pouco tempo. Assim, o título se tornou o jogo first-party que mais vendeu em menos tempo na história da PlayStation. A novidade foi revelada nesta quarta-feira (23) pela publicadora em seu perfil oficial no Twitter. Lançado em 9 de novembro, o jogo alcançou a marca de 5,1 milhões de cópias vendidas apenas em sua semana de estreia — batendo, também, o recorde de vendas da franquia da Santa Monica Studios no mesmo período.",
+      "photoCover": "https://cdn.ome.lt/jizBaf2WckH4KkdPQ7tR6ZZ-P2k=/1200x630/smart/extras/conteudos/God_of_War_kratos_x_freya_fNFsSps.jpg"
+    },
+    {
+      "id": "2",
+      "photo":"Uncharted",
+      "title":"saiu um novo jogo",
+      "description":"Prepare-se para a aventura mais emocionante e arriscada de Nathan Drake em Uncharted 5: Tesouros Perdidos. A aclamada série da Naughty Dog está de volta com um novo capítulo repleto de ação, mistério e reviravoltas emocionantes.Desta vez, Nathan Drake e sua equipe de exploradores intrépidos embarcam em uma jornada épica em busca de uma lendária cidade perdida nas profundezas da Amazônia.À medida que eles desvendam os segredos da civilização antiga e enfrentam inúmeros desafios, os jogadores serão levados a locais deslumbrantes e exóticos, desde selvas inexploradas até cidades submersas misteriosas. Tesouros Perdidos apresenta um novo sistema de jogabilidade, permitindo aos jogadores escolher entre várias abordagens para resolver quebra-cabeças complexos e enfrentar inimigos ferozes. Os gráficos de última geração, combinados com uma trilha sonora envolvente, mergulharão os jogadores em um mundo vibrante e cheio de detalhes.Além disso, a narrativa cativante mergulhará ainda mais fundo na história de Nathan Drake, revelando segredos de seu passado e forçando-o a enfrentar escolhas difíceis que determinarão o destino da busca pelo tesouro perdido.  Uncharted 5: Tesouros Perdidos promete ser uma experiência inesquecível que levará os jogadores a um mundo de exploração, perigo e descoberta. Esteja pronto para se juntar a Nathan Drake e sua equipe em sua maior aventura até agora!",
+      "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2019/10/nolan-north-a-voz-de-nathan-drake-quer-que-uncharted-5-aconteca.jpg"
+    }
+    
+]
