f033638dfa606bc3148da60f4c31398d840d1716
lups-717 <lcastilho717@gmail.com>
2023-10-17 10:53:19 -0300

    content created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -12,6 +12,21 @@ export const dataFake = [
       "title":"saiu um novo jogo",
       "description":"Prepare-se para a aventura mais emocionante e arriscada de Nathan Drake em Uncharted 5: Tesouros Perdidos. A aclamada série da Naughty Dog está de volta com um novo capítulo repleto de ação, mistério e reviravoltas emocionantes.Desta vez, Nathan Drake e sua equipe de exploradores intrépidos embarcam em uma jornada épica em busca de uma lendária cidade perdida nas profundezas da Amazônia.À medida que eles desvendam os segredos da civilização antiga e enfrentam inúmeros desafios, os jogadores serão levados a locais deslumbrantes e exóticos, desde selvas inexploradas até cidades submersas misteriosas. Tesouros Perdidos apresenta um novo sistema de jogabilidade, permitindo aos jogadores escolher entre várias abordagens para resolver quebra-cabeças complexos e enfrentar inimigos ferozes. Os gráficos de última geração, combinados com uma trilha sonora envolvente, mergulharão os jogadores em um mundo vibrante e cheio de detalhes.Além disso, a narrativa cativante mergulhará ainda mais fundo na história de Nathan Drake, revelando segredos de seu passado e forçando-o a enfrentar escolhas difíceis que determinarão o destino da busca pelo tesouro perdido.  Uncharted 5: Tesouros Perdidos promete ser uma experiência inesquecível que levará os jogadores a um mundo de exploração, perigo e descoberta. Esteja pronto para se juntar a Nathan Drake e sua equipe em sua maior aventura até agora!",
       "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2019/10/nolan-north-a-voz-de-nathan-drake-quer-que-uncharted-5-aconteca.jpg"
-    }
+    },
+    {
+      "id": "3",
+      "photo":"GOW",
+      "title":"Marvel's Spiderman 2 está trazendo novidades interessantes",
+      "description":"Depois de quase dois anos sem grandes novidades, a Sony finalmente mostrou o que a gente pode esperar do novo Marvel’s Spider-Man 2 — principalmente em termos de história e jogabilidade. A sequência chega ao PlayStation 5 neste ano e os mais de 10 minutos de gameplay apresentados durante o PlayStation Showcase deram uma boa noção das melhorias e adições que a continuação vai trazer.                      A chegar de Kraven, o Caçador como o vilão principal da aventura chamou muito a atenção, mas quem roubou mesmo a cena foi o icônico uniforme negro do Homem-Aranha. Como já tinha sido antecipado no teaser de anúncio, o simbionte será parte fundamental do novo jogo e agora vimos que isso não vai se limitar apenas ao enredo, mas também nas mecânicas de combate e exploração.",
+      "photoCover": "https://i.ytimg.com/vi/bgqGdIoa52s/maxresdefault.jpg"
+    },
+    {
+      "id": "4",
+      "photo":"GOW",
+      "title":"God Of War Ragnarok ganha como melhor trilha sonora",
+      "description":'Em uma noite repleta de emoções e homenagens à indústria dos videogames, o The Game Awards de 2022 brilhou com a entrega do prestigioso prêmio de Melhor Trilha Sonora. A aclamada trilha sonora "Blood upon the Snow" levou para casa a honra, deixando os fãs de jogos e amantes da música encantados.              Composta por um talentoso e inovador músico, "Blood upon the Snow" provou ser a trilha sonora perfeita para acompanhar a experiência de um dos jogos mais emocionantes do ano. A música, que incorpora elementos épicos e emocionais, aprofunda a imersão dos jogadores no enredo, proporcionando uma atmosfera envolvente e cativante.                                                                           O prêmio de Melhor Trilha Sonora no The Game Awards é um reconhecimento tanto para o compositor quanto para a equipe de desenvolvimento do jogo que escolheu essa obra-prima musical para enriquecer a experiência dos jogadores. "Blood upon the Snow" é uma prova do impacto duradouro que uma trilha sonora excepcional pode ter nos videogames, elevando-os à categoria de obras de arte completas. Parabéns a todos os envolvidos nesta emocionante conquista!',
+      "photoCover": "https://consequence.net/wp-content/uploads/2022/11/hozier-blood-upon-the-snow-god-of-war-ragnarok-video-game-soundtrack-song.jpg"
+    },
+
     
 ]
